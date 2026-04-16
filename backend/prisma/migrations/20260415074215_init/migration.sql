8e3c5fe566d308f09689325bad0f068a460fd3b6
Emeka  Oramalu <143879651+Kingmeka33@users.noreply.github.com>
2026-04-16 03:12:29 +0200

    Add files via upload

---
+++ backend/prisma/migrations/20260415074215_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `products` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `category` VARCHAR(191) NULL,
+    `stock_quantity` INTEGER NOT NULL,
+    `image_url` VARCHAR(2048) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
